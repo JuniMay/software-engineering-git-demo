d2c6bc98cd77bf776e1b1e7dc0dfb9551a03ec43
Junyi Mei <juni_may@outlook.com>
2024-05-31 10:31:55 +0800

    update app.py in B2

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from math import isnan
 
 # This is a line of modification
 
+# MODIFY IN B2!!!!
+
 app = Flask(__name__)
 CORS(app)
 # https://stackoverflow.com/questions/27766794/switching-from-sqlite-to-mysql-with-flask-sqlalchemy
